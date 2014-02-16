bb1db92bea514b233b076f537f93f00e34207be8
revanthb3000 <revanthb3000@gmail.com>
2014-02-16 11:43:01 +0530

    Modified script to filter messages in a better manner.

--- convs/chatFilter.py
+++ convs/chatFilter.py
@@ -9,6 +9,7 @@ for fileName in os.listdir(os.getcwd()):
         if("Stranger:" in matter):
             matter = matter.replace("\n","<br/>")
             matter = matter[0:matter.find("Stranger has disconnected.")]
+            matter = matter[0:matter.find("You have disconnected.")]
             fp.write("<td><tr><p style=\"color:black;margin-left:20px;font-size:25px\">" + matter.replace("You:","<font color='blue'>You:</font>").replace("Stranger:","<font color='red'>Stranger:</font>") + "</p></td></tr>")
         fp.write("<br/><br/>")
 
